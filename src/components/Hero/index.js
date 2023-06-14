57c252c0a30cf6462f924747bb64a59e8d3bc582
nhannt <nhannt.gm@gmail.com>
2023-06-14 19:15:20 +0700

    [fix] hero background

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -18,7 +18,7 @@ const Hero = () => {
             return (
               <Paper key={index + 1}>
                 <div className='movie-card-container'>
-                  <div className='movie-card' style={{ '--img': `url("${movie?.backdrop && movie?.backdrop[0]}")` }}>
+                  <div className='movie-card' style={{ '--img': `url("${movie?.backdrops && movie?.backdrops[0]}")` }}>
                     <div className='movie-detail'>
                       <div className='movie-poster'>
                         <img src={movie.poster} alt='' />
