a389c4b4f7430c6436c7842e08c33b3940b4e9d2
nhannt <nhannt.gm@gmail.com>
2023-06-14 19:01:08 +0700

    add review feature

--- src/api/addReview.js
+++ src/api/addReview.js
@@ -1,9 +1,21 @@
 import api from './axiosConfig'
 
-const addReview = async (id) => {
+const addReview = async (accessToken, id, message) => {
   try {
-    const review = await api.post(`/api/v1/reviews/${id}`)
-  } catch (error) {}
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    }
+
+    const data = JSON.stringify(message)
+
+    const response = await api.post(`/api/v1/reviews/${id}`, data, { headers })
+
+    return response.data
+  } catch (error) {
+    console.log('add review error: ', error)
+    return error
+  }
 }
 
 export default addReview
