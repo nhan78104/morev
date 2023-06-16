6ecab8b17ea4a41d40cbf52ff0c0f6de52ebecbb
nhannt <nhannt.gm@gmail.com>
2023-06-16 08:01:42 +0700

    [fix] fix some chore

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,15 @@
+import { Result } from 'antd'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function NotFound() {
-  return <div>NotFound</div>
+const NotFound = () => {
+  return (
+    <div className='forget-password-container'>
+      <div className='success-container'>
+        <Result status='error' title='This page not found!' subTitle={<Link to='/'>Back to home.</Link>} />
+      </div>
+    </div>
+  )
 }
 
 export default NotFound
